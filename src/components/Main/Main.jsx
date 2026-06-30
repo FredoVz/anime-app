08d84f7e0eac8bfd1134eacfefda160e574d77ed
FredoVz <dm.fredo018@gmail.com>
2026-06-30 16:06:15 +0700

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main({ children }) {
+  return (
+    <main className="main">
+      {children}
+      {/* <Box animes={animes} onSelectedAnime={handleSelectedAnime} />
+      <SelectedBox selectedAnime={selectedAnime} /> */}
+    </main>
+  );
+}
